5b3dc501423874fe445fd09ba0bfc9060b092180
Dan Andoni <d.andoni@rosoftlab.net>
2016-04-24 23:10:45 +0200

    Added alertor protocol

---
+++ Happy Shop/Tools/Alertor/AlertorProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  AlertorProtocol.swift
+//  Happy Shop
+//
+//  Created by Andoni Dan on 21/04/16.
+//  Copyright © 2016 Andoni Dan. All rights reserved.
+//
+
+import Foundation
+
+protocol AlertorProtocol {
+    
+    func noInternet()
+}
