bf2a10ea3ca39e118d2e8cb7fc9d137b01507154
Dan Andoni <d.andoni@rosoftlab.net>
2016-04-24 23:08:50 +0200

    Changed Alertor minion folder

---
+++ Happy Shop/Tools/Alertor/AlertorMinion.swift
@@ -0,0 +1,16 @@
+//
+//  AlertorMinion.swift
+//  Happy Shop
+//
+//  Created by Andoni Dan on 21/04/16.
+//  Copyright © 2016 Andoni Dan. All rights reserved.
+//
+
+import Foundation
+
+class AlertorMinion: AlertorProtocol {
+    
+    func noInternet() {
+        Alertor.noInternet()
+    }
+}
